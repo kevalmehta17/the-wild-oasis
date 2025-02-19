410c9f81b57ace2c0670b10a174b4b413b1fe5c4
Keval <kevalmehta885@gmail.com>
2025-02-19 11:38:34 +0530

    Header & Image Modified

--- app/layout.js
+++ app/layout.js
@@ -3,11 +3,10 @@ export const metadata = {
   description: "Experience the ultimate luxury at The Wild Oasis, a premier cabin hotel nestled in nature's embrace. Enjoy unparalleled comfort and breathtaking views.",
 };
 
-import Logo from "@/app/_components/Logo";
-import Navigation from "@/app/_components/Navigation";
 import "@/app/_styles/globals.css";
 
 import { Josefin_Sans } from "next/font/google";
+import Header from "./_components/Header";
 
 const josefin = Josefin_Sans({
   subsets: ["latin"],
@@ -18,14 +17,11 @@ console.log(josefin);
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${josefin.className} text-primary-100 bg-primary-950 min-h-screen`}>
-        <header>
-          <Logo />
-          <Navigation />
-        </header>
-
-        <main>{children}</main>
-        <footer>Copyright by Me</footer>
+      <body className={`${josefin.className} antialiased text-primary-100 bg-primary-950 min-h-screen flex flex-col`}>
+        <Header />
+        <div className="flex-1 px-8 py-12">
+          <main className="max-w-7xl mx-auto bg-red-400">{children}</main>
+        </div>
       </body>
     </html>
   );
