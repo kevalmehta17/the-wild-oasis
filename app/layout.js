d3e3f0c4281007f33af29bef0202673e45c09422
Keval <kevalmehta885@gmail.com>
2025-02-19 11:07:56 +0530

    Provided Font style

--- app/layout.js
+++ app/layout.js
@@ -7,10 +7,18 @@ import Logo from "@/app/_components/Logo";
 import Navigation from "@/app/_components/Navigation";
 import "@/app/_styles/globals.css";
 
+import { Josefin_Sans } from "next/font/google";
+
+const josefin = Josefin_Sans({
+  subsets: ["latin"],
+  display: "swap",
+})
+console.log(josefin);
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="text-primary-100 bg-primary-950 min-h-screen">
+      <body className={`${josefin.className} text-primary-100 bg-primary-950 min-h-screen`}>
         <header>
           <Logo />
           <Navigation />
